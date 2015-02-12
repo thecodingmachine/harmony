a3b2d6b281fc8a68267caeddf5900e6f68e5a437
David Négrier <d.negrier@thecodingmachine.com>
2015-02-12 17:34:09 +0100

    Removing composer related files

--- composer-harmony.sh
+++ composer-harmony.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # This runs the internal composer install for mouf (instead of the default composer.json file that is supposed to be used by other frameworks)
-COMPOSER=composer-mouf.json php composer.phar $@
+COMPOSER=composer-harmony-core.json php composer.phar $@
--- src-dev/Mouf/Commands/ConsoleApplication.php
+++ src-dev/Mouf/Commands/ConsoleApplication.php
@@ -33,7 +33,7 @@ class ConsoleApplication extends Application implements
 	public function getEmbeddedComposer() {
 		// Check where autoload would be
 		
-		putenv("COMPOSER=composer-mouf.json");
+		putenv("COMPOSER=composer-harmony-core.json");
 		
 		if (!$classLoader = @include __DIR__.'/../../../vendor/autoload.php') {
 			throw new MoufException('You must set up the project dependencies. Did you skip plugins when installing Mouf via Composer?');
