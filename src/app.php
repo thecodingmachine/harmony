b08f16166be83c737dbb981c8a9102d83f135991
David Négrier <d.negrier@thecodingmachine.com>
2015-05-12 13:04:30 +0200

    Adding support for modules autogeneration: app does not fail if modules.php is not commited

--- src/app.php
+++ src/app.php
@@ -7,7 +7,11 @@ if (file_exists(__DIR__.'/../vendor-harmony/autoload.php')) {
     require_once __DIR__.'/../vendor-harmony/autoload.php';
 }
 
-$moduleDescriptors = require __DIR__.'/../modules.php';
+if (file_exists(__DIR__.'/../modules.php')) {
+    $moduleDescriptors = require __DIR__.'/../modules.php';
+} else {
+    $moduleDescriptors = require __DIR__.'/../modules.init.php';
+}
 
 $modules = array_map(function ($descriptor) { return $descriptor['module']; }, $moduleDescriptors);
 
